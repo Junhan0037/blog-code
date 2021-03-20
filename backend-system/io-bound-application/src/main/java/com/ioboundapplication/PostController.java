a42feea57675aa97f5c573465c5006756885568f
Junhan0037 <wnsgks0037@naver.com>
2021-03-20 21:31:12 +0900

    feat: I/O bound 애플리케이션
    - 프로젝트 추가

---
+++ backend-system/io-bound-application/src/main/java/com/ioboundapplication/PostController.java
@@ -0,0 +1,27 @@
+package com.ioboundapplication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    @PostMapping("/post")
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPostList() {
+        return postRepository.findAll();
+    }
+
+}
